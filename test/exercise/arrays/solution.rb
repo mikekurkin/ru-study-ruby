9f05193f2d55db63e03ec222688c92d5a2100469
Mike Kurkin <mike@kurkin.net>
2022-10-05 21:58:36 +0300

    solved replace

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,7 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map{ |el| el.positive? ? array.max : el }
       end
 
       def search(_array, _query)
